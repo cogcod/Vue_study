28beaa9dead2bafdb23480554ee923f680abffdc
yura seol <yricog1290@gmail.com>
2022-06-03 08:49:14 +0900

    structure

---
+++ src/views/ContactView.vue
@@ -0,0 +1,23 @@
+<template>
+  <HeaderCont />
+  <main id="main">
+    <section class="contact__cont">
+      <div class="contact__inner"></div>
+    </section>
+  </main>
+  <FooterCont />
+</template>
+
+<script>
+import HeaderCont from "@/components/HeaderCont.vue";
+import FooterCont from "@/components/FooterCont.vue";
+
+export default {
+  components: {
+    HeaderCont,
+    FooterCont,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/ReferView.vue
@@ -0,0 +1,29 @@
+<template>
+  <HeaderCont />
+  <main id="main">
+    <section class="reference__cont">
+      <TitleCont name1="Reference" name2="Book" />
+      <div class="reference__inner"></div>
+      <ContactCont />
+    </section>
+  </main>
+  <FooterCont />
+</template>
+
+<script>
+import HeaderCont from "@/components/HeaderCont.vue";
+import FooterCont from "@/components/FooterCont.vue";
+import TitleCont from "@/components/TitleCont.vue";
+import ContactCont from "@/components/ContactCont.vue";
+
+export default {
+  components: {
+    HeaderCont,
+    FooterCont,
+    TitleCont,
+    ContactCont,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
